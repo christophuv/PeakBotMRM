02741fb4b5fec64ed1d8b837c3d4fb1d722747c1
christoph <cbueschl@cemm.at>
2022-08-22 12:57:02 +0200

    version increment

--- src/PeakBotMRM/PeakBotMRM.py
+++ src/PeakBotMRM/PeakBotMRM.py
@@ -38,7 +38,7 @@ class Config(object):
     """Base configuration class"""
 
     NAME    = "PeakBotMRM"
-    VERSION = "0.9.38"
+    VERSION = "0.9.40"
 
     RTSLICES       = 255   ## should be of 2^n-1
     NUMCLASSES     =   2   ## [Peak, noPeak]
